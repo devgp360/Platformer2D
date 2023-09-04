18f2b7b3100af778a893623b8cebcbc89eaaa773
Carlos <cammend.net@gmail.com>
2023-09-04 09:38:05 -0600

    Agregando palmera para escenarios

---
+++ scenes/game/levels/objects/back_palm_tree/back_palm_tree.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+
+# Función de carga del nodo
+func _ready():
+	self.play()
+	#if type == "Left":
+	#elif type == "Right":
+	#	animation.play("idle_right")
+	#else:
+	#	animation.play("idle_regular")
