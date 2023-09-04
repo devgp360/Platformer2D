785bd1e017e7da819b83f1e1472b2e264d6d617b
Carlos <cammend.net@gmail.com>
2023-09-04 11:32:14 -0600

    Palmera frontal

--- scenes/game/levels/objects/back_palm_tree/back_palm_tree.gd
+++ scenes/game/levels/objects/back_palm_tree/back_palm_tree.gd
@@ -3,8 +3,3 @@ extends AnimatedSprite2D
 # Función de carga del nodo
 func _ready():
 	self.play()
-	#if type == "Left":
-	#elif type == "Right":
-	#	animation.play("idle_right")
-	#else:
-	#	animation.play("idle_regular")
