2f9850ad66df5ece9c379f3bb855909512aaec0b
Maxim Feshchuk <masimka@users.noreply.github.com>
2023-09-04 12:09:30 -0600

    parallax

---
+++ scenes/game/levels/rooms/scene_1/Camera2D.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+
+var _direction = 'right'
+var _step = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(self.position.x)
+	if (self.position.x < 0 and _direction == 'left') || (self.position.x < 1500 and _direction == 'right'):
+		_direction = 'right'
+	elif self.position.x > 1500 and _direction == 'right':
+		_direction = 'left'
+		
+	if _direction == 'right':
+		self.position.x = self.position.x + _step
+	else:
+		self.position.x = self.position.x - _step
