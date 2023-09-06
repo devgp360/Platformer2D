1ec1e95198e851e082dbf2e8728fda4fc9f97cec
Maxim Feshchuk <masimka@users.noreply.github.com>
2023-09-06 08:43:29 -0600

    objeto cañoñ

--- scenes/game/levels/objects/pickable_object/pickable_object.gd
+++ scenes/game/levels/objects/pickable_object/pickable_object.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 ## Clase que controla animación y configuración de los objetos recolectables
 ##
 ## Setea la animación del objeto segun el nombre configurado
@@ -6,13 +6,16 @@ extends Node2D
 
 
 # Nombre del personaje principal
-@export var animation = ''
+@export_enum(
+	"blue_diamond",
+	"green_diamond", 
+	"red_diamond",
+	"gold_coin",
+	"silver_coin",
+) var animation: String
 
 # Definimos el sprite animado de la moneda
-@onready var _animated_sprite = $test/AnimatedSprite2D
-
-# Nombre del personaje principal
-var _player_name = 'MainCharacter'
+@onready var _animated_sprite = $AnimatedSprite2D
 
 
 # Función de carga del nodo
@@ -25,7 +28,7 @@ func _ready():
 	var _animation2 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/02.png"
 	var _animation3 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/03.png"
 	var _animation4 = "res://assets/sprites/treasure_hunters/pirate_treasure/sprites/" + animation + "/04.png"
-	print(animation)
+
 	# Aplicamos la textura cargada a la animación
 	_animated_sprite.sprite_frames.set_frame("idle", 0, load(_animation1))
 	_animated_sprite.sprite_frames.set_frame("idle", 1, load(_animation2))
@@ -35,27 +38,20 @@ func _ready():
 	# Reproducimos la animación idle
 	_animated_sprite.play("idle")
 	
-	
-# Función que siempre se llama
-func _process(_delta):
-	# Validamos si presionamos el boton Escape
-	if Input.is_action_pressed("ui_cancel"):
-		# Cambiamos la animación
-		_animated_sprite.play("taken")
-
-
-func _physics_process(delta):
-	pass
-	# Obtenemos todos los objetos que collapsan con la moneda
-	#var bodies = get_overlapping_bodies()
-	# Recorremos todos los objetos
-	#for body in bodies:
-		# Validamos si el objeto es nuestro personaje principal
-		#if body.name == _player_name:
-			# Reproducimos la animación de la moneda recogida
-			#_animated_sprite.play("taken")
-
 
 func _on_animated_sprite_2d_animation_finished():
 	# Eliminamos el objeto recogido de la escena
 	queue_free()
+	
+	
+func do_animation():
+	if animation == "gold_coin" or animation == "silver_coin":
+			_animated_sprite.play("coin_taken")
+	else:
+		_animated_sprite.play("diamond_taken")
+
+
+func _on_area_entered(area):
+	if area.is_in_group("player"):
+		# Reproducimos la animación de la moneda recogida
+		do_animation()
--- scenes/game/levels/objects/taking_damage_object/taking_damage_object.gd
+++ scenes/game/levels/objects/taking_damage_object/taking_damage_object.gd
@@ -9,15 +9,9 @@ extends Area2D
 # Definimos la escena de destrucción del objeto
 @onready var _box_destroyed = $BoxDestroyed
 
-# Ruta de sprites de animación
-var _base_link = "res://assets/sprites/treasure_hunters/merchant_ship/sprites/"
 	
 # Función de carga del nodo
 func _ready():
-	# Cargamos las texturas de animación según el nombre configurado
-	var _animation = _base_link + "box/idle" + "/1.png"
-	# Aplicamos la textura cargada a la animación
-	_animated_sprite.sprite_frames.set_frame("idle", 0, load(_animation))
 	# Reproducimos la animación idle
 	_animated_sprite.play("idle")
 	# No mostramos la animación de destrucción
@@ -35,20 +29,9 @@ func _on_animated_sprite_2d_animation_finished():
 		await get_tree().create_timer(3).timeout
 		# Eliminamos el objeto
 		queue_free()
-		
-			
+					
 	
 func do_animation():
-	# Cargamos las imágenes de animación pegar
-	var _animationHit2 = _base_link + "box/hit" + "/2.png"
-	var _animationHit3 = _base_link + "box/hit" + "/3.png"
-	var _animationHit4 = _base_link + "box/hit" + "/4.png"
-	
-	# Aplicamos la textura cargada a la animación
-	_animated_sprite.sprite_frames.set_frame("hit", 0, load(_animationHit2))
-	_animated_sprite.sprite_frames.set_frame("hit", 1, load(_animationHit3))
-	_animated_sprite.sprite_frames.set_frame("hit", 2, load(_animationHit4))
-	# Reproducimos la animación
 	_animated_sprite.play("hit")
 
 
