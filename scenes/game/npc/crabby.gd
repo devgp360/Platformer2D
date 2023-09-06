10701dc50cd199636a896098d97b11459feac84e
Maxim Feshchuk <masimka@users.noreply.github.com>
2023-09-06 15:53:58 -0600

    movimientos NPC

--- scenes/game/npc/crabby.gd
+++ scenes/game/npc/crabby.gd
@@ -4,11 +4,32 @@ extends CharacterBody2D
 ## Setea la animación y comportamiento del NPC 
 
 
+# Acciones del NPC
+@export_enum(
+	"idle",
+	"run", 
+) var animation: String
+
 # Variable para control de animación
 @onready var _animation := $NpcAnimation
+
+var _gravity = 10
+var _speed = 25
+var _velocity = Vector2(0,0)
 
 
 # Función de inicialización
 func _ready():
-	_animation.play("idle")
+	if not animation:
+		return
+	_animation.play(animation)
 	
+	
+func _process(delta):
+	_move_character()
+	
+	
+func _move_character():
+	_velocity.y += _gravity 
+	_velocity.x += - _speed 
+	_velocity = move_and_slide()
