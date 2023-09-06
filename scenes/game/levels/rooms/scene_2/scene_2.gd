ac58907c5994389613402865aec0344044dbfb7a
Carlos <cammend.net@gmail.com>
2023-09-06 15:50:31 -0600

    Cambio de nivel usando señales

--- scenes/game/levels/rooms/scene_2/scene_2.gd
+++ scenes/game/levels/rooms/scene_2/scene_2.gd
@@ -1,9 +1,23 @@
 extends Node2D
+## Script de escena principal.
+##
+## Tiene variables y funciones relacionados con la escena, por ejemplo cambios hacia otras escenas
+## Cambio de escenas: https://docs.google.com/document/d/1eIBtgr8wln1pT0aZ4c-YWk_pqngyBg4HDsgdYLAXv28/edit?usp=sharing
+## Uso de señales: https://docs.google.com/document/d/1vFSOuJkBy7xr5jksgCBNaTpqJHE_K87ZNafB5ZJ_I0M/edit?usp=sharing
 
 
-# Función para detectar eventos de teclado o ratón
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_up"):
-		# Cuando presionemos la "flecha arriba" cambiamos de escena
+# Area para siguiente nivel
+@onready var _area_next_level = $Areas/AreaNextLevel
+
+
+# Función de inicialización
+func _ready():
+	# Escuchamos cuando el personaje entre al área de contacto
+	_area_next_level.body_entered.connect(_load_nex_level)
+
+
+# Cargamos el siguiente nivel (la siguiente escena)
+func _load_nex_level(body):
+	if body.is_in_group("player"):
 		var scene = "res://scenes/game/levels/rooms/scene_1/scene_1.tscn"
 		SceneTransition.change_scene(scene)
