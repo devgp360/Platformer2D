e4af3d83444ae58a1971eb8f68a95281e0834f86
Maxim Feshchuk <masimka@users.noreply.github.com>
2023-09-07 09:30:18 -0600

    movimiento passivo

--- scenes/game/characters/main_character/main_character_collision.gd
+++ scenes/game/characters/main_character/main_character_collision.gd
@@ -1,4 +1,7 @@
 extends Node2D
+## Controlador de colisiones.
+##
+## Detecta eventos de colisión
 
 @export var character: CharacterBody2D
 
@@ -10,8 +13,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Revisamos todas las colisiones
 	for i in character.get_slide_collision_count():
+		# Obtenemos la colisión
 		var collision = character.get_slide_collision(i)
+		# Obtenemos el collider
 		var collider = collision.get_collider()
+		# Validamos si existe el metodo hit
 		if collider and collider.has_method("hit"):
 			collider.hit()
