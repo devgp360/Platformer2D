2ec01346284d7d21b2a80cac8172f7f2ea7a0ebf
Maxim Feshchuk <masimka@users.noreply.github.com>
2023-09-11 12:04:45 -0600

    muerte

---
+++ scenes/game/levels/objects/death/death.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		# Quitamos vidas
+		var _move_script = body.get_node("MainCharacterMovement")
+		_move_script.hit(100)
